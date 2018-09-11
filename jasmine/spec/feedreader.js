2b0080d7fbd12082ee388a9e5f13bf9bfcfc4182
Meo Hu <mayoh727@gmail.com>
2018-09-11 13:52:55 -0700

    Add expectation: body has class menu-hidden to be true

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -102,6 +102,7 @@ $(function() {
       menu.click();
       expect(body.classList.contains('menu-hidden')).toBe(false);
       menu.click();
+      expect(body.classList.contains('menu-hidden')).toBe(true);
     });
   });
 
